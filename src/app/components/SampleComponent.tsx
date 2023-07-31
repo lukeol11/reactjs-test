d649b3d42c92d395bc67f0ee968bf5f71c30e8fc
Luke O'Leary <luke.oleary1@gmail.com>
2023-07-31 20:42:36 +0100

    feat: Added getTime function to new component

--- src/app/components/SampleComponent.tsx
+++ src/app/components/SampleComponent.tsx
@@ -1,8 +1,15 @@
 export default function SampleComponent(props: any) {
+
+    const getTime = () => {
+        const date = new Date()
+        return date.toLocaleTimeString()
+    }
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1 className="text-6xl">{props.heading}</h1>
         <p className="text-xl">{props.content}</p>
+        <p className="text-xl">{getTime()}</p>
         </main>
     )
 }
